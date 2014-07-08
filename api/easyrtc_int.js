bb807aef1e0a12bd2e68b9a6b65a1bba0fa23b5e
Rod Apeldoorn <rod.apeldoorn@priologic.com>
2014-07-08 15:42:13 -0700

    version bump

--- api/easyrtc_int.js
+++ api/easyrtc_int.js
@@ -1,5 +1,5 @@
 /** @class
- *@version 1.0.12
+ *@version 1.0.12-beta
  *<p>
  * Provides client side support for the EasyRTC framework.
  * Please see the easyrtc_client_api.md and easyrtc_client_tutorial.md
@@ -202,7 +202,7 @@ var Easyrtc = function() {
         ALREADY_CONNECTED: "ALREADY_CONNECTED",
         "BAD_CREDENTIAL": "BAD_CREDENTIAL"
     };
-    this.apiVersion = "1.0.12";
+    this.apiVersion = "1.0.12-beta";
     /** Most basic message acknowledgment object */
     this.ackMessage = {msgType: "ack"};
     /** Regular expression pattern for user ids. This will need modification to support non US character sets */
