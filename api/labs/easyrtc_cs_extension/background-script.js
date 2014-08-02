f7260c5caa8d32845df14f5df66eb220165d01f0
Harold Thetiot <hthetiot@gmail.com>
2014-08-02 12:20:34 -0700

    easyrtc screen share example #87

---
+++ api/labs/easyrtc_cs_extension/background-script.js
@@ -0,0 +1,35 @@
+﻿// this background script is used to invoke desktopCapture API
+// to capture screen-MediaStream.
+
+var session = ['screen', 'window'];
+var pending = false;
+
+chrome.runtime.onConnect.addListener(function (port) {
+    port.onMessage.addListener(portOnMessageHanlder);
+    
+    // this one is called for each message from "content-script.js"
+    function portOnMessageHanlder(message) {
+        if(message == 'get-sourceId' && !pending) {
+            pending = true;
+            chrome.desktopCapture.chooseDesktopMedia(session, port.sender.tab, onAccessApproved);
+        }
+    }
+
+    // on getting sourceId
+    // "sourceId" will be empty if permission is denied.
+    function onAccessApproved(sourceId) {
+
+        pending = false;
+        
+        // if "cancel" button is clicked
+        if(!sourceId || !sourceId.length) {
+            return port.postMessage('PermissionDeniedError');
+        }
+        
+        // "ok" button is clicked; share "sourceId" with the
+        // content-script which will forward it to the webpage
+        port.postMessage({
+            chromeMediaSourceId: sourceId
+        });
+    }
+});
