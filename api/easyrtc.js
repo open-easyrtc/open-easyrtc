3aeb8a66f34f1927115b784ce6c67dff1e6677a6
Rod Apeldoorn <rod.apeldoorn@priologic.com>
2014-08-06 11:16:57 -0700

    Removing -beta from versions in prep for release.

--- api/easyrtc.js
+++ api/easyrtc.js
@@ -182,7 +182,7 @@ if (!window.createIceServer) {
         return {'url': url, 'credential': credential, 'username': username};
     };
 }/** @class
- *@version 1.0.12-beta
+ *@version 1.0.12
  *<p>
  * Provides client side support for the EasyRTC framework.
  * Please see the easyrtc_client_api.md and easyrtc_client_tutorial.md
@@ -398,7 +398,7 @@ var Easyrtc = function() {
         ALREADY_CONNECTED: "ALREADY_CONNECTED",
         "BAD_CREDENTIAL": "BAD_CREDENTIAL"
     };
-    this.apiVersion = "1.0.12-beta";
+    this.apiVersion = "1.0.12";
     /** Most basic message acknowledgment object */
     this.ackMessage = {msgType: "ack"};
     /** Regular expression pattern for user ids. This will need modification to support non US character sets */
--- api/easyrtc_int.js
+++ api/easyrtc_int.js
@@ -1,5 +1,5 @@
 /** @class
- *@version 1.0.12-beta
+ *@version 1.0.12
  *<p>
  * Provides client side support for the EasyRTC framework.
  * Please see the easyrtc_client_api.md and easyrtc_client_tutorial.md
@@ -215,7 +215,7 @@ var Easyrtc = function() {
         ALREADY_CONNECTED: "ALREADY_CONNECTED",
         "BAD_CREDENTIAL": "BAD_CREDENTIAL"
     };
-    this.apiVersion = "1.0.12-beta";
+    this.apiVersion = "1.0.12";
     /** Most basic message acknowledgment object */
     this.ackMessage = {msgType: "ack"};
     /** Regular expression pattern for user ids. This will need modification to support non US character sets */
